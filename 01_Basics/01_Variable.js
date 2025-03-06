d208a77d1443ab9a3e742268b7f3a14075b697f7
Pardeep Singh <87849366+Pardeep404@users.noreply.github.com>
2025-03-06 03:57:24 +0000

    Variable in JS

---
+++ 01_Basics/01_Variable.js
@@ -0,0 +1,13 @@
+const myName = "Pardeep";
+let cityName = "Faridabad";
+var stateName = "Haryana";
+emailId = "pardeep@gmail.com";
+let password
+
+// myName = "Pardeep Singh"; Not Allowed
+cityName = "Hisar";
+stateName = "HR";
+emailId = "pardeep@google.com";
+
+console.table([myName, cityName, stateName, emailId, password] );
+console.log(myName, cityName, stateName, emailId, password );
