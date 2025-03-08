e0a8105085b42d079975825d3e80f5d88d45a51c
Pardeep Singh <87849366+Pardeep404@users.noreply.github.com>
2025-03-08 13:45:34 +0000

    DataTypes in JS

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all JS as newer veersion
+
+//  alert("Hello"); // we are using nodejs, not browser
+
+console.log(7 + 7);
+console.log("Pardeep Singh");
+
+let name = "Pardeep";
+let age = 18+1;
+let isLoggedIn = false;
+let state = null;
+let city;
+
+console.table([typeof name, typeof age, typeof isLoggedIn, typeof state, typeof city])
+console.table([name, age, isLoggedIn, state, city])
+
+// node 01_Basics/02_DataTypes.js
