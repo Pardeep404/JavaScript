8849ef4f8a178219079953d9cd096d44ab2672d7
Pardeep Singh <87849366+Pardeep404@users.noreply.github.com>
2025-03-08 14:22:36 +0000

    Conversion DataType in JS

---
+++ 01_Basics/03_ConversionOperation.js
@@ -0,0 +1,29 @@
+let score = "33abs";
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNum = Number(score);
+console.log(typeof valueInNum);
+console.log(valueInNum);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isloggedIn = "Hi"
+
+let booleanIsLoggedIn = Boolean(isloggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// ""=> false
+// "Hitesh" => true
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+// node 01_Basics/03_ConversionOperation.js
