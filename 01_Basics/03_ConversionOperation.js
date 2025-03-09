fc87bfad0345b864cace79fd52999352d3aafc13
Pardeep Singh <87849366+Pardeep404@users.noreply.github.com>
2025-03-09 14:50:34 +0000

    Data Operation in JS

--- 01_Basics/03_ConversionOperation.js
+++ 01_Basics/03_ConversionOperation.js
@@ -26,4 +26,30 @@ let stringNumber = String(someNumber);
 console.log(stringNumber)
 console.log(typeof stringNumber)
 
+// *********** Operations ***********
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2 + 2)
+console.log(2 - 2)
+console.log(2 * 2)
+console.log(2 ** 2)
+console.log(2 / 2)
+console.log(2 % 2)
+
+let str1 = "Hello"
+let str2 = " Pardeep"
+
+let str3 = str1 + str2;
+console.log(str3)
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+console.log(+true);
+console.log(+"");
+
 // node 01_Basics/03_ConversionOperation.js
