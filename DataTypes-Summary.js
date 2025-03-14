e6177ea6480ecda119d7174fcebadce878fb0401
Pardeep Singh <87849366+Pardeep404@users.noreply.github.com>
2025-03-14 10:58:53 +0000

    Data Types Summary

---
+++ DataTypes-Summary.js
@@ -0,0 +1,24 @@
+// Primitive
+// 7 Types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+let str = "Pardeep"
+let num = 7
+let isLoggedIn = false
+let outSideTemp = null
+let myAge = undefined
+let id = Symbol('123')
+let anotherId = Symbol('123')
+let money = 7834768476476476476476478478478478n
+console.table([typeof str, typeof num, typeof isLoggedIn, typeof outSideTemp, typeof myAge, typeof id, typeof anotherId, id === anotherId, typeof money])
+
+// Reference (Non primitive)
+// Array, Objects, Functions
+
+let arr = ["pardeep", "deepu", "deepak"];
+let obj = { FName: "pardeep", SName: "deepu", TName: "deepak" }
+let fun = function () {
+    console.log("hello, World!")
+}
+console.table([typeof arr, typeof obj, typeof fun])
+
+// node DataTypes-Summary.js
