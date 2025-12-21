f8cbb343eed5c8e86c2787df0ef5600703544f6a
Pardeep Singh <87849366+Pardeep404@users.noreply.github.com>
2025-12-21 12:15:47 +0000

    Operator Behavior

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,13 +1,13 @@
 let score = "Pardeep";
 
 // Checking the type of variable
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 
 // Converting string to number
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
 // Conversion results:
 // "33"     => 33
@@ -19,7 +19,7 @@ let isLoggedIn = "Pardeep";
 
 // Converting value to boolean
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // Boolean conversion rules:
 // 1         => true
@@ -31,5 +31,37 @@ let someNumber = 33;
 
 // Converting number to string
 let stringNumber = String(someNumber);
-console.log(stringNumber);          // "33"
-console.log(typeof stringNumber);   // string
+// console.log(stringNumber);          // "33"
+// console.log(typeof stringNumber);   // string
+
+// ************* Operations *************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "Hello";
+let str2 = " Pardeep";
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(((3 + 4) * 5) % 3);
+
+console.log(+true);
+console.log(+"");
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
