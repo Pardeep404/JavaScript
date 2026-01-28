7892c1c34b6d33e3c072ceff9362e8ff73bba441
Pardeep Singh <87849366+Pardeep404@users.noreply.github.com>
2026-01-28 12:19:38 +0000

    oop

--- 10_classes_&_oop/oop.js
+++ 10_classes_&_oop/oop.js
@@ -2,6 +2,29 @@ const user = {
   username: "Pardeep",
   loginCount: 8,
   signedIn: true,
+
+  getUserDetails: function () {
+    // console.log("Got user details from database")
+    console.log(`Username: ${this.username}`);
+    // console.log(this)
+  },
 };
 
-console.log(user)
+console.log(user.username);
+// console.log(user.getUserDetails());
+// console.log(this)
+
+function User(username, loginCount, isLoggedIn) {
+  this.username = username;
+  this.loginCount = loginCount;
+  this.isLoggedIn = isLoggedIn;
+  this.greeting = function () {
+    console.log(`Welcome ${this.username}`);
+  };
+  return this;
+}
+
+const userOne = new User("Pardeep", 27, true);
+const userTwo = new User("Deepu", 28, false);
+console.log(userOne.constructor);
+// console.log(userTwo);
