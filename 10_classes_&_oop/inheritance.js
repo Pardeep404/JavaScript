c11898bd1bafec7f35de9bdfabe526646dee2a2c
Pardeep Singh <87849366+Pardeep404@users.noreply.github.com>
2026-01-29 00:00:20 +0000

    Inheritance

---
+++ 10_classes_&_oop/inheritance.js
@@ -0,0 +1,32 @@
+class User {
+  constructor(username) {
+    this.username = username;
+  }
+
+  logMe() {
+    console.log(`USERNAME is ${this.username}`);
+  }
+}
+
+class Teacher extends User {
+  constructor(username, email, password) {
+    super(username);
+    this.email = email;
+    this.password = password;
+  }
+
+  addCourse() {
+    console.log(`A new course was addes by ${this.username}`);
+  }
+}
+
+const chai = new Teacher("chai", "chai@gmail.com", "123");
+chai.addCourse()
+
+const tea = new User("greenTea");
+tea.logMe()
+
+console.log(chai === tea)
+console.log(chai === Teacher)
+console.log(chai instanceof Teacher)
+console.log(chai instanceof User)
